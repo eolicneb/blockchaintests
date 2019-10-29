3c33bdb847b7f7a37695abe0073225ce7c5ded11
Nicolas Bueno <eoneb@hotmail.com>
2019-10-29 08:06:24 -0300

    fix/modular-multiplicative-inverse

--- elliptic/ecdsa.py
+++ elliptic/ecdsa.py
@@ -15,6 +15,7 @@ a = 2
 b = 2
 p = 17
 G = (5, 1)
+n = 16
 
 def modular_inv(n):
     n = n%p
@@ -23,30 +24,59 @@ def modular_inv(n):
             return m
     return None
 
+
+def mod_mult_inv_(n, p=p):
+    a, b = p, n%p
+    if a == b:
+        return None
+    rs, qs = [a, b], []
+    while rs[-1] != 0:
+        qs.append(rs[-2]//rs[-1])
+        rs.append(rs[-2] - qs[-1]*rs[-1])    
+    x, y = 1, -(-1)**len(qs)
+    for ix in range(len(qs), 0, -1):
+        x, y = x*qs[ix-1] + y, x
+    return x%p
+
+
+def mod_mult_inv(n, p=p):
+    a, b = p, n%p
+    if 0 == b:
+        return None
+    qs = []
+    while b != 0:
+        qs[:0] = [a//b]
+        a, b = b, a-qs[0]*b
+    x, y = 1, 1
+    for q in qs:
+        x, y = -x*q + y, x
+    return x%p
+
 def add(P: tuple, Q: tuple) -> tuple:
     Px, Py = P
     Px, Py = Px%p, Py%p # (mod p)
     Qx, Qy = Q
     Qx, Qy = Qx%p, Qy%p # (mod p)
-    s = (((Py-Qy)%p)*modular_inv(Px-Qx))%p
+    s = (((Py-Qy)%p)*mod_mult_inv(Px-Qx))%p
     # print("s:", s)
     Rx = int(((s*s)%p-(Px+Qx)%p)%p)
     Ry = int((s*(Px-Rx)-Py)%p)
     return Rx, Ry
 
-for i in range(1,4*p):
-    print(f"modular inversor of {i} (mod {p}): ", modular_inv(i))
+# for i in range(1,p+5):
+#     print(f"modular inversor of {i} (mod {p}): ", modular_inv(i), mod_mult_inv(i), mod_mult_inv_(i))
 
 def times2(P):
     Px, Py = P
     Px, Py = Px%p, Py%p
-    s = (((3*Px*Px+a)%p)*modular_inv(2*Py))%p
-    # print("s:", s, "(2Py)⁻¹(mod p):", modular_inv(2*Py))
+    s = (((3*Px*Px+a)%p)*mod_mult_inv(2*Py))%p
+    # print("s:", s, "(2Py)⁻¹(mod p):", mod_mult_inv(2*Py))
     Rx = int(((s*s)%p-2*Px)%p)
     Ry = int((s*(Px-Rx)-Py)%p)
     return Rx, Ry
 
 def times_k(k):
+    k = (k-1)%n + 1
     if k == 1:
         return G
     Pub = times2(G)
@@ -58,9 +88,17 @@ def times_k(k):
     return Pub
 
 
-for i in range(1,2*p):
+for i in range(1, 2*p):
     try:
         print(i, times_k(i))
     except:
         print(i, "O")
         break
+
+from math import log2
+limit_pow = int(log2(n))+1
+G2s = [G]
+for i in range(1, limit_pow):
+    G2s.append(times2(G2s[-1]))
+    print(2**i, G2s[-1])
+
